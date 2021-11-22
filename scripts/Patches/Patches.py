57c6752f0e78d50ae30b79f5835c36aab6134831
Rob Realmuto <rrealmuto@gmail.com>
2021-11-22 13:43:31 -0500

    Added skulltula count text

--- scripts/Patches/Patches.py
+++ scripts/Patches/Patches.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from Patches.Rom import Rom
 from Patches.Messages import read_messages, update_message_by_id, read_shop_items, update_warp_song_text, \
         write_shop_items, remove_unused_messages, \
-        add_item_messages, repack_messages, shuffle_messages, \
+        add_item_messages, update_item_messages, repack_messages, shuffle_messages, \
         get_message_by_id
 
 #from SaveContext import SaveContext
@@ -20,4 +20,5 @@ def patch_rom(rom:Rom):
     
     
     messages = read_messages(rom)
+    update_item_messages(messages)
     repack_messages(rom, messages)
