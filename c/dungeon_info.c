96f57f4ce61e00063fdc42d27c50a9eae3e1c4ae
Ryan Dorson <slyryd@gmail.com>
2022-03-31 20:54:00 -0700

    Fix dungeon skull count

--- c/dungeon_info.c
+++ c/dungeon_info.c
@@ -245,7 +245,12 @@ void draw_dungeon_info(z64_disp_buf_t *db)
         if (!d->has_tokens)
             continue;
 
-        int8_t tokens = GET_GS_FLAGS(d->index);
+        int8_t token_flags = GET_GS_FLAGS(d->index);
+        int8_t tokens = 0;
+        while (token_flags) {
+            tokens += token_flags & 1;
+            token_flags >>= 1;
+        }
         if (tokens < 0)
             tokens = 0;
         if (tokens > 9)
