44159c3188d2c3e773db210ca9e78b3d01b4f6c7
Ryan Dorson <slyryd@gmail.com>
2022-04-08 02:05:37 -0700

    Add scenes in preparation for heart pieces

--- c/world_map_info.h
+++ c/world_map_info.h
@@ -11,29 +11,58 @@ extern uint32_t gGsFlagsShifts[4];
 
 int8_t get_tokens(uint8_t index);
 
-#define DEKU_TREE 0
-#define DODONGOS_CAVERN 1
-#define JABU_JABU 2
-#define FOREST_TEMPLE 3
-#define FIRE_TEMPLE 4
-#define WATER_TEMPLE 5
-#define SPIRIT_TEMPLE 6
-#define SHADOW_TEMPLE 7
-#define BOTTOM_OF_THE_WELL 8
-#define ICE_CAVERN 9
+#define DEKU_TREE 0x0
+#define DODONGOS_CAVERN 0x1
+#define JABU_JABU 0x2
+#define FOREST_TEMPLE 0x3
+#define FIRE_TEMPLE 0x4
+#define WATER_TEMPLE 0x5
+#define SPIRIT_TEMPLE 0x6
+#define SHADOW_TEMPLE 0x7
+#define BOTTOM_OF_THE_WELL 0x8
+#define ICE_CAVERN 0x9
 
-#define GERUDO_TRAINING_GROUND 11
-#define THIEVES_HIDEOUT 12
-#define INSIDE_GANONS_CASTLE 13
+#define GERUDO_TRAINING_GROUND 0xB
+#define THIEVES_HIDEOUT 0xC
+#define INSIDE_GANONS_CASTLE 0xD
 
-#define GOHMA 17
-#define KING_DODONGO 18
-#define BARINADE 19
-#define PHANTOM_GANON 20
-#define VOLVAGIA 21
-#define MORPHA 22
-#define TWINROVA 23
-#define BONGO_BONGO 24
+#define TREASURE_BOX_SHOP 0x10
+
+#define GOHMA 0x11
+#define KING_DODONGO 0x12
+#define BARINADE 0x13
+#define PHANTOM_GANON 0x14
+#define VOLVAGIA 0x15
+#define MORPHA 0x16
+#define TWINROVA 0x17
+#define BONGO_BONGO 0x18
+
+#define DOG_LADY 0x35
+#define LAKESIDE_LABORATORY 0x38
+#define GROTTOS 0x3E
+#define REDEAD_GRAVE 0x3F
+#define SHOOTING_GALLERY 0x42
+#define DAMPES_GRAVE_WINDMILL 0x48
+#define FISHING_POND 0x49
+#define BOMBCHU_BOWLING_ALLEY 0x4B
+#define RANCH_HOUSE_SILO 0x4C
+#define HOUSE_OF_SKULLTULA 0x50
+
+#define KAKARIKO_VILLAGE 0x52
+#define GRAVEYARD 0x53
+#define ZORAS_RIVER 0x54
+#define KOKIRI_FOREST 0x55
+#define LAKE_HYLIA 0x57
+#define ZORAS_DOMAIN 0x58
+#define ZORAS_FOUNTAIN 0x59
+#define GERUDO_VALLEY 0x5A
+#define LOST_WOODS 0x5B
+#define DESERT_COLOSSUS 0x5C
+#define GERUDOS_FORTRESS 0x5D
+#define HAUNTED_WASTELAND 0x5E
+#define DEATH_MOUNTAIN_TRAIL 0x60
+#define DEATH_MOUNTAIN_CRATER 0x61
+#define GORON_CITY 0x62
 
 void draw_world_map_info();
 
