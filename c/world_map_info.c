8ac272e5225e32fd9ed41951e3b85e33223c4e6a
Ryan Dorson <slyryd@gmail.com>
2022-04-09 01:47:53 -0700

    sub area menu

--- c/world_map_info.c
+++ c/world_map_info.c
@@ -7,28 +7,37 @@
 
 #define PAUSE_MAP 1
 
-typedef struct
-{
-    uint8_t index;
-    char name[15];
-} world_map_area_entry_t;
-
 world_map_area_entry_t world_map_areas[] = {
-    {21, "Wasteland"},
-    {20, "Fortress"},
-    {19, "Gerudo Valley"},
-    {18, "Lake Hylia"},
-    {11, "Lon Lon Ranch"},
-    {14, "Market"},
-    {10, "Hyrule Field"},
-    {15, "Death Mountain"},
-    {16, "Kakariko"},
-    {13, "Lost Woods"},
-    {12, "Kokiri Forest"},
-    {17, "Zoras Domain"},
+    {21, 0, {DESERT_COLOSSUS}, "Waste/Col"},                                                                                  // 4 tokens, 1 HPs
+    {20, 0, {GERUDOS_FORTRESS}, "Fortress"},                                                                                  // 2 tokens, 2 HPs
+    {19, 0, {GERUDO_VALLEY}, "Valley"},                                                                                       // 4 tokens, 2 HPs
+    {18, 0, {LAKESIDE_LABORATORY, FISHING_POND, LAKE_HYLIA}, "Lake"},                                                         // 5 tokens, 3 HPs
+    {11, 0, {RANCH_HOUSE_SILO}, "Ranch"},                                                                                     // 4 tokens, 1 HPs
+    {14, 0, {TREASURE_BOX_SHOP, BOMBCHU_BOWLING_ALLEY, DOG_LADY_HOUSE}, "Market"},                                            // 4 tokens, 3 HPs
+    {10, 0, {GROTTOS}, "Field"},                                                                                              // 2 tokens, 2 HPs
+    {15, 0, {DEATH_MOUNTAIN_TRAIL, DEATH_MOUNTAIN_CRATER, GORON_CITY}, "Death Mtn"},                                          // 8 tokens, 4 HPs
+    {16, 0, {IMPAS_HOUSE, REDEAD_GRAVE, DAMPES_GRAVE_WINDMILL, HOUSE_OF_SKULLTULA, KAKARIKO_VILLAGE, GRAVEYARD}, "Kak Area"}, // 8 tokens, 8 HPs
+    {13, 0, {LOST_WOODS}, "Lost Wds"},                                                                                        // 4 tokens, 2 HPs
+    {12, 0, {}, "Kokiri"},                                                                                                    // 3 tokens, 0 HPs
+    {17, 0, {ZORAS_RIVER, ZORAS_DOMAIN, ZORAS_FOUNTAIN}, "Zora Area"},                                                        // 8 tokens, 7 HPs
 };
 
 int world_map_area_count = array_size(world_map_areas);
+
+world_map_area_entry_t world_map_sub_areas[] = {
+    {21, 1, {}, "Wasteland"},                                                                        // 1 tokens, 0 HPs
+    {21, 1, {DESERT_COLOSSUS}, "Colossus"},                                                          // 3 tokens, 1 HPs
+    {15, 1, {DEATH_MOUNTAIN_TRAIL}, "Trail"},                                                        // 4 tokens, 1 HPs
+    {15, 1, {DEATH_MOUNTAIN_CRATER}, "Crater"},                                                      // 2 tokens, 2 HPs
+    {15, 1, {GORON_CITY}, "Goron Cty"},                                                              // 2 tokens, 1 HPs
+    {16, 0, {IMPAS_HOUSE, DAMPES_GRAVE_WINDMILL, HOUSE_OF_SKULLTULA, KAKARIKO_VILLAGE}, "Kakariko"}, // 8 tokens, 8 HPs
+    {16, 0, {REDEAD_GRAVE, DAMPES_GRAVE_WINDMILL, GRAVEYARD}, "Graveyard"},                          // 8 tokens, 8 HPs
+    {17, 1, {ZORAS_RIVER}, "River"},                                                                 // 4 tokens, 4 HPs
+    {17, 1, {ZORAS_DOMAIN}, "Domain"},                                                               // 1 tokens, 1 HPs
+    {17, 1, {ZORAS_FOUNTAIN}, "Fountain"},                                                           // 3 tokens, 2 HPs
+};
+
+int world_map_sub_area_count = array_size(world_map_sub_areas);
 
 extern uint32_t CFG_WORLD_MAP_INFO_ENABLE;
 
@@ -36,13 +45,13 @@ uint32_t gGsFlagsMasks[] = {0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000};
 uint32_t gGsFlagsShifts[] = {0, 8, 16, 24};
 
 // gs_flags at 8011B46C
-#define GET_GS_FLAGS(index) \
-    ((z64_file.gs_flags[(index) >> 2] & gGsFlagsMasks[(index)&3]) >> gGsFlagsShifts[(index)&3])
+#define GET_GS_FLAGS(area_index) \
+    ((z64_file.gs_flags[(area_index) >> 2] & gGsFlagsMasks[(area_index)&3]) >> gGsFlagsShifts[(area_index)&3])
 
-int8_t get_tokens(uint8_t index)
+int8_t get_tokens(uint8_t area_index)
 {
     int8_t tokens = 0;
-    int32_t token_flags = GET_GS_FLAGS(index);
+    int32_t token_flags = GET_GS_FLAGS(area_index);
     while (token_flags)
     {
         tokens += token_flags & 1;
@@ -90,6 +99,22 @@ void draw_world_map_info(z64_disp_buf_t *db)
 
     db->p = db->buf;
 
+    int areas_center_x = Z64_SCREEN_WIDTH / 4;
+    draw_world_map_areas(db, world_map_areas, world_map_area_count, areas_center_x);
+
+    int sub_areas_center_x = Z64_SCREEN_WIDTH * 3 / 4;
+    draw_world_map_areas(db, world_map_sub_areas, world_map_sub_area_count, sub_areas_center_x);
+
+    // Finish
+
+    text_flush(db);
+
+    gDPFullSync(db->p++);
+    gSPEndDisplayList(db->p++);
+}
+
+void draw_world_map_areas(z64_disp_buf_t *db, world_map_area_entry_t *areas, int area_count, int center_x)
+{
     // Call setup display list
 
     gSPDisplayList(db->p++, &setup_db);
@@ -100,11 +125,11 @@ void draw_world_map_info(z64_disp_buf_t *db)
     int padding = 1;
     int bg_width =
         padding +
-        (14 * font_sprite.tile_w) + // world map area names
+        (9 * font_sprite.tile_w) + // world map area names
         padding +
         (4 * (icon_size + padding)); // skull, skull count, hp, hp count
-    int bg_height = padding + (world_map_area_count * (icon_size + padding));
-    int bg_left = (Z64_SCREEN_WIDTH - bg_width) / 2;
+    int bg_height = padding + (area_count * (icon_size + padding));
+    int bg_left = center_x - (bg_width / 2);
     int bg_top = (Z64_SCREEN_HEIGHT - bg_height) / 2;
 
     int left = bg_left + padding;
@@ -130,20 +155,27 @@ void draw_world_map_info(z64_disp_buf_t *db)
 
     // Draw area names
 
-    for (int i = 0; i < world_map_area_count; i++)
+    for (int i = 0; i < area_count; i++)
     {
-        world_map_area_entry_t *w = &(world_map_areas[i]);
+        world_map_area_entry_t *w = &(areas[i]);
         int top = start_top + ((icon_size + padding) * i) + 1;
-        text_print(w->name, left, top);
+        if (w->is_sub_area)
+        {
+            text_print(w->name, left, top);
+        }
+        else
+        {
+            text_print(w->name, left, top);
+        }
     }
 
-    left += (14 * font_sprite.tile_w) + padding;
+    left += (9 * font_sprite.tile_w) + padding;
 
     // Draw skull sprite
 
     sprite_load(db, &quest_items_sprite, 11, 1);
 
-    for (int i = 0; i < world_map_area_count; i++)
+    for (int i = 0; i < area_count; i++)
     {
         int top = start_top + ((icon_size + padding) * i);
         sprite_draw(db, &quest_items_sprite, 0,
@@ -154,16 +186,23 @@ void draw_world_map_info(z64_disp_buf_t *db)
 
     // Draw skull count
 
-    for (int i = 0; i < world_map_area_count; i++)
+    for (int i = 0; i < area_count; i++)
     {
-        world_map_area_entry_t *w = &(world_map_areas[i]);
+        world_map_area_entry_t *w = &(areas[i]);
 
-        int8_t tokens = get_tokens(w->index);
+        int8_t tokens = get_tokens(w->area_index);
 
         char count[2] = "0";
         count[0] += (tokens % 10);
         int top = start_top + ((icon_size + padding) * i) + 1;
-        text_print(count, left, top);
+        if (w->is_sub_area)
+        {
+            text_print("-", left, top);
+        }
+        else
+        {
+            text_print(count, left, top);
+        }
     }
 
     left += icon_size + padding;
@@ -172,7 +211,7 @@ void draw_world_map_info(z64_disp_buf_t *db)
 
     sprite_load(db, &quest_items_sprite, 13, 1);
 
-    for (int i = 0; i < world_map_area_count; i++)
+    for (int i = 0; i < area_count; i++)
     {
         int top = start_top + ((icon_size + padding) * i);
         sprite_draw(db, &quest_items_sprite, 0,
@@ -183,24 +222,26 @@ void draw_world_map_info(z64_disp_buf_t *db)
 
     // Draw small hp counts
 
-    for (int i = 0; i < world_map_area_count; i++)
+    for (int i = 0; i < area_count; i++)
     {
-        world_map_area_entry_t *w = &(world_map_areas[i]);
+        world_map_area_entry_t *w = &(areas[i]);
 
-        int8_t hps = 1; // z64_file.world_map_area_keys[w->index];
+        int8_t hps = 1; // z64_file.world_map_area_keys[w->area_index];
 
         char count[2] = "0";
         count[0] += (hps % 10);
         int top = start_top + ((icon_size + padding) * i) + 1;
+        if (w->is_sub_area)
+        {
+            text_print(count, left, top);
+        }
+        else
+        {
+            text_print("-", left, top);
+        }
+
         text_print(count, left, top);
     }
 
     left += icon_size + padding;
-
-    // Finish
-
-    text_flush(db);
-
-    gDPFullSync(db->p++);
-    gSPEndDisplayList(db->p++);
 }
--- c/world_map_info.h
+++ c/world_map_info.h
@@ -37,7 +37,8 @@ int8_t get_tokens(uint8_t index);
 #define TWINROVA 0x17
 #define BONGO_BONGO 0x18
 
-#define DOG_LADY 0x35
+#define DOG_LADY_HOUSE 0x35
+#define IMPAS_HOUSE 0x37
 #define LAKESIDE_LABORATORY 0x38
 #define GROTTOS 0x3E
 #define REDEAD_GRAVE 0x3F
@@ -64,6 +65,33 @@ int8_t get_tokens(uint8_t index);
 #define DEATH_MOUNTAIN_CRATER 0x61
 #define GORON_CITY 0x62
 
-void draw_world_map_info();
+typedef struct
+{
+    struct
+    {
+        uint8_t is_chest : 1;
+        uint8_t is_collect : 1;
+        uint8_t is_event_chk_inf : 1;
+        uint8_t is_item_get_inf : 1;
+        uint8_t is_inf_table : 1;
+    };
+    uint8_t scene;
+    uint8_t byte_offset;
+    uint8_t bit_values;
+} flag_info_t;
+
+typedef struct
+{
+    uint8_t area_index;
+    struct
+    {
+        uint8_t is_sub_area : 1;
+    };
+    uint8_t hp_scenes[6];
+    char name[10];
+} world_map_area_entry_t;
+
+void draw_world_map_info(z64_disp_buf_t *db);
+void draw_world_map_areas(z64_disp_buf_t *db, world_map_area_entry_t *areas, int area_count, int center_x);
 
 #endif
